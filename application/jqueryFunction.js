5e003fc9f3f0b574bd75fda5d3b46f746581d64e
Fahg2541 <Fahg2541@gmail.com>
2020-05-04 01:10:42 +0700

    Popup

--- application/jqueryFunction.js
+++ application/jqueryFunction.js
@@ -62,6 +62,50 @@
             .fail(function (jqxhr, textStatus, error) {
             })
     }
+    function moveroom(){
+        var name = $('#inputName').val();
+        var email = $('#inputEmail').val();
+        var message = $('#inputMessage').val();
+        if(name.trim() == '' ){
+            alert('Please enter your name.');
+            $('#inputName').focus();
+            return false;
+        }else if(email.trim() == '' ){
+            alert('Please enter your email.');
+            $('#inputEmail').focus();
+            return false;
+        }else if(email.trim() != '' && !reg.test(email)){
+            alert('Please enter valid email.');
+            $('#inputEmail').focus();
+            return false;
+        }else if(message.trim() == '' ){
+            alert('Please enter your message.');
+            $('#inputMessage').focus();
+            return false;
+        }else{
+            $.ajax({
+                type:'POST',
+                url:'submit_form.php',
+                data:'contactFrmSubmit=1&name='+name+'&email='+email+'&message='+message,
+                beforeSend: function () {
+                    $('.submitBtn').attr("disabled","disabled");
+                    $('.modal-body').css('opacity', '.5');
+                },
+                success:function(msg){
+                    if(msg == 'ok'){
+                        $('#inputName').val('');
+                        $('#inputEmail').val('');
+                        $('#inputMessage').val('');
+                        $('.statusMsg').html('<span style="color:green;">Thanks for contacting us, we\'ll get back to you soon.</p>');
+                    }else{
+                        $('.statusMsg').html('<span style="color:red;">Some problem occurred, please try again.</span>');
+                    }
+                    $('.submitBtn').removeAttr("disabled");
+                    $('.modal-body').css('opacity', '');
+                }
+            });
+        }
+    }
 
     function saveRoom(){
             var api_url = "http://localhost/php/g5/api.php/updateRoom/";
